6ca4ea29f4ba8a27945090d73cfd65d4df4a5a4f
mol1986@naver.com <kileab@naver.com>
2022-11-16 19:47:28 +0900

    (2022.11.16) 비트 연산자

--- Source.c
+++ Source.c
@@ -2,58 +2,27 @@
 
 void main()	
 {
-    // 산술 연산자
-    /*
-    int A = 10;
-    int B = 20;
+  	// 2진수를 10진수로 나타내는 방법
+    // 1 byte에 2 진수로 저장된 값을 2의 제곱으로 나타냅니다.
 
-    int C = A + B; 
-    int D = A - B;
-    int E = A * B;
-    int F = A / B;
+    // 비트 연산자란?
+    // 비트 단위로 논리 연산을 수행하기 위해 사용하는 연산자입니다.
+    int x = 10; // 0000 1010
+    int y = 3;  // 0000 0011
 
-    // 서식 지정자의 경우 하나의 표준 출력 함수에 여러 개의 서식을 넣을 수 있으며
-    // 서식 지정자의 순서는 왼쪽에서부터 시작합니다.
-    printf("C의 값 : %d \nD의 값 : %d \nE의 값 : %d \nF의 값 : %d\n", C, D, E, F);
+    printf("x와 y를 AND 연산한 결과 : %d\n", x & y);
+    printf("x와 y를 OR 연산한 결과 : %d\n", x | y);
+    printf("x와 y를 XOR 연산한 결과 : %d\n", x ^ y);
 
-    // 내가 출력하고 싶은 변수의 형태와 서식 지정자가 일치해야 정확한 값이 출력됩니다.
-    printf("A의 값 : %c", A);
-    */
+    printf("x의 NOT 연산한 결과 : %d\n", ~x);
 
-    // 과제
-    /*
-    // 1. (int value1) 변수 = 변수 + 심볼릭 상수
-    // 2. (int value2) 변수 = 변수 * 리터럴 상수
-    // 3. (int value3) 변수 = 심복릭 상수 - 심볼릭 상수
-    // 4. (int value4) 변수 = 심볼릭 상수 / 리터럴 상수
-    // 5. (int value5) 변수 = 리터럴 상수 * 리터럴 상수
+    // 자료형     메모리 크기    값의 범위
+    // char        1 byte    -128 ~ 127
+    // short       2 byte   -32768 ~ 32767
+    // int         4 byte   -2,147,483,648 ~ 2,147,483,647
 
-    int variable = 10;
-    const int constVariable1 = 20;
-    const int constVariable2 = 30;
-
-    int value1 = variable + constVariable1;
-    int value2 = variable * 5;
-    int value3 = constVariable1 - constVariable2;
-    int value4 = constVariable1 / 5;
-    int value5 = 10 * 10;
-
-    printf("value1의 값 : %d\n", value1);
-    printf("value2의 값 : %d\n", value2);
-    printf("value3의 값 : %d\n", value3);
-    printf("value4의 값 : %d\n", value4);
-    printf("value5의 값 : %d\n", value5);
-    */
-
-    // 비트 (bit)
-    // 데이터를 나타내는 최소의 단위이며, 0 또는 1이라는 값만 저장할 수 있습니다.
-
-    // 바이트 (byte)
-    // 비트가 8개 모여서 이루어진 단위로 하나의 문자를 나타나기 위한 최소의 단위입니다.
-
-    // 10진수를 2진수로 변환하는 과정
-    // 10진수를 1이 될 때까지 계속 2로 나누어 준 다음 나눈 위치의 나머지
-    // 값을 아래에서 위로 순서대로 정렬합니다.
+    // float       4 byte   3.4E-38(-3.4*10^38) ~ 3.4E+38(3.4*10^38) (7digits)
+    // double      8 byte   1.79E-308(-1.79*10^308) ~ 1.79E+308(1.79*10^308) (15digits)
 
 
 
