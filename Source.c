dfd6149d3f1177b9e39122f8f60743c8dd6e96df
mol1986@naver.com <kileab@naver.com>
2022-11-25 19:46:48 +0900

    (2022.11.25) 시프트 연산자

--- Source.c
+++ Source.c
@@ -2,75 +2,71 @@
 
 void main()	
 {
-	// 증감 연산자
-	// 피연산자를 1씩 증가 혹은 감소시킬 때 
-	// 사용하는 연산자입니다.
-
-	// 전위 증감 연산자
+	// 2중 for문
 	/*
-	// 먼저 값을 증가시키거나 감소시킨 후에 
-	// 변수에 값을 넣어주는 연산자입니다.
-	int x = 10;
-	int result = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("내부에 있는 for문\n");
+		}
 
-	// result(11) = (11) <- 10 + 1 
-	result = ++x;
-
-	printf("result 변수의 값 : %d\n", result);
-
-	// result(10) = (10) <- 11 - 1
-	result = --x;
-
-	printf("result 변수의 값 : %d\n", result);
+		printf("외부에 있는 for문\n");
+	}
 	*/
 
-	// 후위 증감 연산자
+	// 구구단
 	/*
-	// 값을 먼저 변수에 넣어준 다음, 증가시키거나
-	// 감소시키는 연산자입니다.
-	int x = 10;
-	int result = 0;
+	for (int i = 2; i < 10; i++)
+	{
+		for (int j = 1; j < 10; j++)
+		{
+			printf("%d x %d : %d\n", i, j, i * j);
+		}
 
-	result = x++;
+		printf("\n");
+	}
+	*/
 
-	printf("result 변수의 값 : %d\n", result);
-	printf("x 변수의 값 : %d\n", x);
+	// continue문
+	/*
+	// 현재 반복에서 명령문의 실행을 종료하고 반복문의
+	// 처음으로 돌아가서 반복문의 다음 코드를 실행하는 명령문입니다.
+	for (int i = 0; i < 5; i++)
+	{
+		if (i == 3)
+		{
+			break;
+		}
 
-	result = x--;
+		printf("i의 값 : %d\n", i);
+	}
+	*/
+
+	// 별 생성 알고리즘
+	/*
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("☆");
+		}
+
+		printf("\n");
+	}
+	*/
+
+	// 시프트 연산자
+	// 비트 값을 주어진 숫자만큼 부호 방향으로 이동시키는 연산자입니다.
 	
-	printf("result 변수의 값 : %d\n", result);
-	printf("x 변수의 값 : %d\n", x);
-	*/
-
-	// 반복문
-	// 프로그램 내에서 특정한 작업을 반복적으로 
-	// 수행하는 명령문입니다.
-
-	// for 문이란?
-	/*
-	// 초기식을 연산하여 조건식의 결과에 따라 
-	// 특정한 횟수만큼 반복하는 반복문입니다.
-
-	// for문의 조건식이 처음부터 거짓이면 실행되지 않습니다.
+	char value = 10; // 0000 1010
 	
-	int value = 0;
+	printf("비트 왼쪽으로 3번 이동한 결과 : %d\n", value << 3); // 0101 0000 
+	
+	printf("비트 오른쪽으로 2번 이동한 결과 : %d\n", value >> 2); // 0000 0010
 
-	   // 초기식; 조건식; 증감식;
-	for (int i = value; i < 5; i++)
-	{
-		// for문에서 조건식이 계속 참인 상태이면,
-		// for문 안에 있는 내용이 무한 반복합니다.
-		printf("Quest\n");
-	}
-	*/
+	// 시프트 연산자에서 대입 연산을 해야 변수에 값이 적용됩니다.
+	printf("value 값 : %d\n", value);
 
-	for (int i = 1; i <= 5; i++)
-	{
-		printf("%d\n", i);
-	}
 
-	for(int i = 5; i >= 1; i--)
-	{
-		printf("%d\n", i);
-	}
 } 
