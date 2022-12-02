da6d7408315a6f437f22c05d6ae468155d5bd0e2
mol1986@naver.com <kileab@naver.com>
2022-12-02 19:46:15 +0900

    (2022.12.2) 문자열

---
+++ (2022.12.1) 배열.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <stdio.h>
+
+void main()
+{
+    // 입력 함수
+    /*
+    // 표준 입력 함수로, 여러 종류의 데이터를 다양한 서식에 맞추어
+    // 입력해주는 함수입니다.
+
+    char alphabet = 'A';
+    int value = 0;
+    float data = 0.0;
+
+    // 출력 <- [][][][][][] <- 입력
+    // 버퍼 오버플로가 발생할 수 있습니다.
+    scanf_s("%c", &alphabet);
+    scanf_s("value = %d data = %f", &value, &data);
+
+    printf("alphabet의 값 : %c\n", alphabet);
+    printf("value의 값 : %d\n", value);
+    printf("data의 값 : %f\n", data);
+    */
+
+    // 배열
+    /*
+    // 같은 자료형의 변수들로 이루어진 유한 집합입니다.
+
+    int space[5] = { 5, };
+
+    // 정해진 배열의 크기를 벗어나서 데이터를 넣을 수 없습니다.
+    // 배열의 모든 인덱스 값으로 0으로 초기화하려면 {0,} 선언해주시면 됩니다.
+    // 배열에 값을 넣지 않으면 0으로 채워지게 됩니다.
+    space[2] = 10;
+
+    // 배열의 경우 첫 번째 원소는 0부터 시작합니다.
+    for (int i = 0; i < 5; i++)
+    {
+        printf("space[%d]의 값 : %d\n", i, space[i]);
+    }
+
+    // 배열은 원하는 원소에 원하는 값을 저장할 수 있으며,
+    // 배열의 크기는 컴파일이 되는 시점부터 고정된 메모리 공간을 가지게 됩니다.
+    // space[5] = 10;
+
+    // 배열의 이름 배열의 시작 주소를 의미합니다.
+    printf("space 배열의 주소 : %p\n", space);
+    printf("space 배열의 0 번째 주소 : %p\n", &space[0]);
+    printf("space 배열의 1 번째 주소 : %p\n", &space[1]);
+    printf("space 배열의 2 번째 주소 : %p\n", &space[2]);
+    printf("space 배열의 3 번째 주소 : %p\n", &space[3]);
+    printf("space 배열의 4 번째 주소 : %p\n", &space[4]);
+    */
+
+}
--- Source.c
+++ Source.c
@@ -2,50 +2,82 @@
 
 void main()	
 {
-	// 입력 함수
-    /*
-    // 표준 입력 함수로, 여러 종류의 데이터를 다양한 서식에 맞추어
-    // 입력해주는 함수입니다.
+	// 문자열
+	// 연속적인 메모리 공간에 저장된 문자 변수의 집합입니다.
+	
+	// 배열의 크기를 생략하고 데이터를 넣게 되면 컴파일러가 자동
+	// 으로 배열의 크기를 계산합니다.
+	
+	// string [A] [B] [C] [D] [E] [\0]
+	char string [ ] = "ABCDE";
 
-    char alphabet = 'A';
-    int value = 0;
-    float data = 0.0;
+	char * charPoint = "ABCDE";
 
-    // 출력 <- [][][][][][] <- 입력
-    // 버퍼 오버플로가 발생할 수 있습니다.
-    scanf_s("%c", &alphabet);
-    scanf_s("value = %d data = %f", &value, &data);
+	// 문자열의 경우 포인터를 이용하여 문자열 상수를 가리키도록 
+	// 할 수 있으며, 문자열 상수는 데이터 영역의 읽기 전용 
+	// 공간에 저장되기 때문에 문자열의 값을 변경할 수 없습니다.
 
-    printf("alphabet의 값 : %c\n", alphabet);
-    printf("value의 값 : %d\n", value);
-    printf("data의 값 : %f\n", data);
-    */
+	// charPoint[3] = 'G';
 
-    // 배열
-    // 같은 자료형의 변수들로 이루어진 유한 집합입니다.
+	charPoint = "BBDDEEGG";
+	charPoint[0] = 'A';
+    // 문자열 생성할 때는 NULL문자가 포함됩니다.
+	// NULL문자의 크기는 1 Byte입니다.
+	//printf("string 배열의 크기 : %d\n", sizeof(string));
 
-    // 정해진 배열의 크기를 벗어나서 데이터를 넣을 수 없습니다. 
-    // 배열의 모든 인덱스 값으로 0으로 초기화하려면 {0,} 선언해주시면 됩니다.
-    // 배열에 값을 넣지 않으면 0으로 채워지게 됩니다.
-    int space[5] = { 5,};
+	// 문자열은 공백도 함께 메모리 공간에 포함하여 크기가 결정되며
+	// , 마지막에 문자열의 끝을 알려주는 제어 문자가 추가됩니다.
 
-    // 배열의 경우 첫 번째 원소는 0부터 시작합니다.
-    for (int i = 0; i < 5; i++)
-    {
-        printf("space[%d]의 값 : %d\n", i, space[i]);
-    }
+	//char * charPtr = string;
 
-    // 배열은 원하는 원소에 원하는 값을 저장할 수 있으며,
-    // 배열의 크기는 컴파일이 되는 시점부터 고정된 메모리 공간을 가지게 됩니다.
-    // space[5] = 10;
+	//printf("charPtr의 크기 : %d", sizeof(charPtr));
 
-    // 배열의 이름 배열의 시작 주소를 의미합니다.
-    printf("space 배열의 주소 : %p\n", space);
-    printf("space 배열의 0 번째 주소 : %p\n", &space[0]);
-    printf("space 배열의 1 번째 주소 : %p\n", &space[1]);
-    printf("space 배열의 2 번째 주소 : %p\n", &space[2]);
-    printf("space 배열의 3 번째 주소 : %p\n", &space[3]);
-    printf("space 배열의 4 번째 주소 : %p\n", &space[4]);
+	//charPtr[1] = 'Z';
+	//charPtr[3] = 'T';
+
+	//* charPtr = 'O';
+	//string[3] = '\0';
+
+	//charPtr = &string[4];
+	//* charPtr = 'M';
+
+	// %s : 문자열을 한번에 출력하는 서식 지정자입니다.
+	//printf("%s\n", string);
+
+	// 문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만,
+	// 문자 배열 사이에 무효의 문자를 넣게 되면 무효의 문자까지만
+	// 문자열을 출력합니다. 
+	//for (int i = 0; i < 5; i++)
+	//{
+		//printf("%c ", string[i]);
+	//}
+
+	// sizeof 연산자
+	/*
+	// 자료형의 크기를 반환하는 연산자입니다.
+	printf("char 데이터의 크기 : %d\n", sizeof(char));
+	printf("short 데이터의 크기 : %d\n", sizeof(short));
+
+	int value = 10;
+	long data = 10;
+
+	// sizeof 연산자는 데이터를 Byte 단위로 표시합니다.
+	printf("int 데이터의 크기 : %d\n", sizeof(value));
+	printf("long 데이터의 크기 : %d\n", sizeof(data));
+
+    
+	// 접미사를 선언해서 리터럴 상수의 자료형 크기를 명시적
+	// 선언합니다.
+	printf("float 데이터의 크기 : %d\n", sizeof(3.14f));
+
+	// 접미사
+	// 실수를 선언할 때 
+	// 3.14f (float)
+	// 3.65 (double)
+
+	printf("double 데이터의 크기 : %d\n", sizeof(3.65));
+	*/
+
 
 
 } 
