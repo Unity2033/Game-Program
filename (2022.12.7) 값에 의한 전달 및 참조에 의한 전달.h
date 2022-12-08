1bc849ed735f962ad43eeaedec84b2d9b044f120
mol1986@naver.com <kileab@naver.com>
2022-12-08 19:47:36 +0900

    (2022.12.8) 재귀 함수

---
+++ (2022.12.7) 값에 의한 전달 및 참조에 의한 전달.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <stdio.h>
+
+void Calculator(int x)
+{
+	x = 1000;
+}
+
+void Swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+
+	printf("x의 주소 : %p\n", x);
+	printf("y의 주소 : %p\n", y);
+
+	printf("x의 값 : %d\n", *x);
+	printf("y의 값 : %d\n", *y);
+}
+
+
+int main()
+{
+	// call by value (값에 의한 전달)
+	/*
+	// 함수 호출 시 넘기는 인자의 값이 매개변수에
+	// 복사되어서 함수 내에서 매개변수에 직접적인
+	// 데이터를 변경해도 인수에 영향을 주지 않는 전달입니다.
+	int value = 0;
+
+	Calculator(value); // <- 값이 반환된 거입니다.
+
+	printf("value의 값 : %d", value);
+	*/
+
+	// call by reference (참조에 의한 전달)
+	// 함수 호출 시 인수로 변수의 주솟값을 전달하여
+	// 함수 내부에서 변경된 사항을 함수 외부에서도
+	// 영향을 끼칠 수 있도록 전달하는 방법입니다.
+
+	int A = 10;
+	int B = 20;
+
+	Swap(&A, &B);
+
+	printf("A의 주소 : %p\n", &A);
+	printf("B의 주소 : %p\n", &B);
+
+	printf("A의 값 : %d\n", A);
+	printf("B의 값 : %d\n", B);
+
+	return 0;
+}
--- Source.c
+++ Source.c
@@ -1,48 +1,63 @@
 ﻿#include <stdio.h>
 
-void Calculator(int x)
+// 재귀 함수
+/*
+void Function(int x)
 {
-	x = 1000;
-}
+	if (x == 0)
+	{
+		return;
+	}
 
-void Swap(int * x, int * y)
+	// 재귀 함수는 함수를 계속 호출하기 때문에 스택 영역에
+	// 메모리가 계속 쌓이게 되므로 스택 오버플로우가 일어납니다.
+	printf("함수 실행\n");
+	Function(x - 1);
+
+	// 재귀 함수의 경우 특정한 시점에서 함수를 반환해야 하며, 
+	// 재귀적으로 호출한 함수는 스택 프레임에 의해 마지막에 호출된 
+	// 함수부터 차례대로 스택 영역에서 해제됩니다.
+}
+*/
+
+int Factorial(int x)
 {
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-
-	printf("x의 주소 : %p\n", x);
-	printf("y의 주소 : %p\n", y);
-
-	printf("x의 값 : %d\n", *x);
-	printf("y의 값 : %d\n", *y);
+	if (x == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return x * Factorial(x - 1);
+	}
 }
-
 
 int main()	
 {
-	// call by value (값에 의한 전달)
+	// 재귀 함수
 	/*
-	// 함수 호출 시 넘기는 인자의 값이 매개변수에
-	// 복사되어서 함수 내에서 매개변수에 직접적인
-	// 데이터를 변경해도 인수에 영향을 주지 않는 전달입니다.
-	int value = 0;
+	// 어떤 함수에서 자신을 다시 호출하여 작업을 
+	// 수행하는 함수입니다.
+	int count = 5;
 
-	Calculator(value); // <- 값이 반환된 거입니다.
-			   
-	printf("value의 값 : %d", value);
+	Function(count);
+	*/
+		
+	// 삼항 연산자
+	/*
+	int value1 = 10;
+	int value2 = 20;
+
+	//           조건          참일 때 거짓일 때
+	printf("%d", value1 != value2 ? 1 : 0);
 	*/
 
-	int A = 10;
-	int B = 20;
+	// 팩토리얼
+	int value = 0;
 
-	Swap(&A, &B);
-
-	printf("A의 주소 : %p\n", &A);
-	printf("B의 주소 : %p\n", &B);
-
-	printf("A의 값 : %d\n", A);
-	printf("B의 값 : %d\n", B);
+	scanf_s("%d", &value);
+				 
+	printf("%d",Factorial(value));
 
 	return 0;
 } 
