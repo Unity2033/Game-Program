9ea7a495ba321ad0553e43cc0e08ba2a1a10a928
mol1986@naver.com <kileab@naver.com>
2022-12-16 19:46:50 +0900

    (2022.12.16) 범용 포인터

--- Source.c
+++ Source.c
@@ -1,34 +1,79 @@
 ﻿#include <stdio.h>
-#include <stdlib.h> // 동적으로 할당할 수 있는 malloc 함수 라이브러리
+#include <limits.h> // 자료형 최댓값과 최솟값을 출력해주는 헤더 파일
+
+// char, int, double
+void Generic(char * name, void * value)
+{
+	if (name == "char")
+	{ 
+		printf("%c\n", *(char*)value);
+	}
+	else if (name == "int")
+	{
+		printf("%d\n", *(int*)value);
+	}
+	else if (name == "double")
+	{
+		printf("%f\n", *(double*)value);
+	}
+}
 
 int main()	
 {
-	// 시작 프로그램으로 설정된 프로젝트의 이름은
-	// 시작 프로그램으로 설정되지 않은 프로젝트의
-	// 이름보다 조금 더 굵은 글씨로 표시됩니다.
-	printf("Lecture 프로젝트의 파일\n");
+	//범용 포인터란?
+	/*
+    // 자료형이 정해지지 않은 상태로 모든 자료형을 저장할
+	// 수 있는 포인터입니다.
 
-	// 동적 할당이란?
-	// 프로그램을 실행 중에 필요한 만큼 메모리를
-	// 할당하는 작업입니다.
-	char * pointer = malloc(sizeof(char));
+	char alphabet = 'A'; // 1 byte
+	int integer = 10; // 4 byte
+	double pi = 3.14; // 8 byte
 
-	// char     
-	// [] -----> [] / [][][] / [][][][] 
-		
-	//동적 할당은 실행 시간에 가변적으로 메모리의 크기를 변경시킬 수 있으며,
-	// 동적으로 메모리의 크기를 할당할 때 바이트 단위로 지정합니다.
-	//*pointer = NULL;
+	void * ptr = &alphabet;
 
-	*pointer = 10;
-	printf("동적 할당한 메모리의 변경 전 주소 : %p\n", pointer);
-	printf("동적 할당한 메모리의 값 : %d\n", *pointer);
+	// 범용 포인터는 메모리 주소에 접근해서 값을 변경할 수 없습니다.
 
-	pointer = pointer + 10;
-	*pointer = 128;
+	printf("ptr이 가리키는 값 : %c\n", *(char *)ptr);
+	printf("ptr의 가리키는 주소 : %p\n", ptr);
 
-	printf("동적 할당한 메모리의 변경 후 주소 : %p\n", pointer);
-	printf("동적 할당한 메모리의 값 : %d\n", *pointer);
+	ptr = &integer;
+	printf("ptr이 가리키는 값 : %d\n", *(int*)ptr);
+	printf("ptr의 가리키는 주소 : %p\n", ptr);
+
+	ptr = &pi;
+	printf("ptr이 가리키는 값 : %f\n", *(double*)ptr);
+	printf("ptr의 가리키는 주소 : %p\n", ptr);
+
+	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가 가리키는
+	// 변수의 자료형으로 형 변환을 해주어야 합니다.
+
+	Generic("char", &alphabet); // 문자 'A'
+	Generic("int", &integer);   // 정수 10
+	Generic("double", &pi);     // 실수 3.14
+	*/
+
+    // 최대값과 최솟값
+	int max = 0;
+	int min = INT_MAX;
+
+	int data[ ] = { 6,10,1,2,8 };
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (data[i] > max)
+		{
+			max = data[i];
+		}
+
+		if (min >= data[i])
+		{
+			min = data[i];
+		}
+	}
+
+	printf("data의 최댓값 : %d\n", max);
+	printf("data의 최솟값 : %d\n", min);
+
 
 
 	return 0;
