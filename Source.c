5022fc462bd22b3566be2e0bfa65bfebeae2ee1f
mol1986@naver.com <kileab@naver.com>
2022-12-19 19:48:46 +0900

    (2022.12.19) typedef

--- Source.c
+++ Source.c
@@ -1,80 +1,111 @@
 ﻿#include <stdio.h>
-#include <limits.h> // 자료형 최댓값과 최솟값을 출력해주는 헤더 파일
 
-// char, int, double
-void Generic(char * name, void * value)
+// 열거형이란?
+// 반드시 하나의 값만 가지게 될 변수들의 집합체입니다.
+
+// 열거형은 정수값으로 정의할 수 없습니다.
+enum State
 {
-	if (name == "char")
-	{ 
-		printf("%c\n", *(char*)value);
-	}
-	else if (name == "int")
-	{
-		printf("%d\n", *(int*)value);
-	}
-	else if (name == "double")
-	{
-		printf("%f\n", *(double*)value);
-	}
-}
+// enum은 정수값이 들어가게 되며, 따로 초기화를 해주지 
+// 않았을 때 가장 위에 있는 멤버 값이 0으로 초기화됩니다.
+   IDLE,    // 대기 상태
+   ATTACK = 10,	// 공격 상태
+   DEFENSE,	// 방어 상태
+   DEATH    // 죽은 상태
+  
+// 열거형 멤버 중에서 값을 변경하게 되면 그 뒤에 있는 열거형
+// 멤버들도 같이 값이 바뀝니다.
+};
+
+// typedef 
+// 이미 사용되는 자료형을 다른 새로운 자료형으로 
+// 재정의해주는 키워드입니다.
+
+typedef int INT32; // int == INT32
+
+typedef struct Player
+{
+	int x;
+	int y;
+	const char * shape;
+}PLAYER; // typedef로 정의할 때는 이름을 다르게 정의해야 합니다.
+
+typedef enum Color
+{
+	BLACK,
+	BLUE
+}COLOR;
 
 int main()	
 {
-	//범용 포인터란?
+	// enum
 	/*
-    // 자료형이 정해지지 않은 상태로 모든 자료형을 저장할
-	// 수 있는 포인터입니다.
+	enum State state;
 
-	char alphabet = 'A'; // 1 byte
-	int integer = 10; // 4 byte
-	double pi = 3.14; // 8 byte
+	// 캐릭터 상태를 지정해줄 수 있습니다.
+	state = ATTACK;
 
-	void * ptr = &alphabet;
+	// enum은 상수이기 때문에 값을 바꿀 수 없습니다.
+	// IDLE = 99;
+	
+	//printf("state의 값 : %d", state);
 
-	// 범용 포인터는 메모리 주소에 접근해서 값을 변경할 수 없습니다.
-
-	printf("ptr이 가리키는 값 : %c\n", *(char *)ptr);
-	printf("ptr의 가리키는 주소 : %p\n", ptr);
-
-	ptr = &integer;
-	printf("ptr이 가리키는 값 : %d\n", *(int*)ptr);
-	printf("ptr의 가리키는 주소 : %p\n", ptr);
-
-	ptr = &pi;
-	printf("ptr이 가리키는 값 : %f\n", *(double*)ptr);
-	printf("ptr의 가리키는 주소 : %p\n", ptr);
-
-	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가 가리키는
-	// 변수의 자료형으로 형 변환을 해주어야 합니다.
-
-	Generic("char", &alphabet); // 문자 'A'
-	Generic("int", &integer);   // 정수 10
-	Generic("double", &pi);     // 실수 3.14
+	switch (state)
+	{
+		case IDLE :	printf("대기 상태");
+			break;
+		case ATTACK : printf("공격 상태");
+			break;
+		case DEFENSE : printf("방어 상태");
+			break;	
+		case DEATH : printf("죽음 상태");
+			break;
+	}
 	*/
 
-    // 최대값과 최솟값
-	int max = 0;
-	int min = INT_MAX;
+	// typedef
+	/*
+	int value1 = 10;
+	INT32 value2 = 20;
 
-	int data[ ] = { 6,10,1,2,8 };
+	COLOR color;
+	color = BLUE;
 
-	for (int i = 0; i < 5; i++)
+	PLAYER player;
+	player.shape = 'A';
+	*/
+
+	// 소수
+	// 1보다 큰 자연수 중 1과 자기 자신만을 약수로 가지는 수
+
+	// 1. 정수값 입력
+	int value = 0;
+	int flag = 0;
+
+	scanf_s("%d", &value);
+
+	for (int i = 2; i < value; i++)
 	{
-		if (data[i] > max)
+		if (value % i == 0)
 		{
-			max = data[i];
-		}
-
-		if (min >= data[i])
-		{
-			min = data[i];
+			flag = 1;
+			break;
 		}
 	}
 
-	printf("data의 최댓값 : %d\n", max);
-	printf("data의 최솟값 : %d\n", min);
+	if (flag == 0)
+	{
+		printf("소수");
+	}
+	else
+	{
+		printf("소수가 아닙니다.");
+	}
 
 
+	// 소수(O) vs 소수(X)
+
+	// 2,3,5,7,11,13,17,19,23,29,31
 
 	return 0;
 } 
