e8150302ff7121c64d74f3c01a38483227923ed7
mol1986@naver.com <kileab@naver.com>
2022-12-21 19:48:19 +0900

    (2022.12.21) 키 입력 함수

--- Source.c
+++ Source.c
@@ -1,72 +1,67 @@
 ﻿#include <stdio.h>
-#include <time.h>
+#include <windows.h> // window에 관련된 헤더 파일
 
 int main()	
 {
-	// rand( )
+	// Blocking 형태의 키 입력
 	/*
-	// 0 ~ 32767 사이의 난수 값을 반환하는
-	// 함수입니다. 
-
-	// time.h 
-    // 1970년 1월 1일 0시 0분 0초부터 경과되는.
-	// 시간을 초(sec)로 반환하는 함수
-
-	srand(time(NULL));
-
-	for (int i = 0; i < 10; i++)
+	while (1)
 	{
-		// rand()함수는 프로그램이 생성될 때 정해진
-		// 값이 출력되기 때문에 프로그램을 여러 번 실행시켜도
-		// 계속 같은 값이 출력됩니다.   
-		int value = rand() % 10 + 1;
-		printf("value의 값 : %d\n", value);
-	}
+		      	   // 224
+		int key = _getch(); // 키를 입력을 했을 때 224라는 값이 반환됩니다.
+		                     
+		// key <- ↑(72) 
+		key = _getch();
+
+		switch (key)
+		{
+			// UP KEY
+			case 72: printf("↑\n");
+				break;
+			// LEFT KEY
+			case 75: printf("←\n");
+				break;
+			// RIGHT KEY
+			case 77: printf("→\n");
+				break;
+			// DOWN KEY
+			case 80: printf("↓\n");
+				break;
+		}
+		
+		printf("게임 애니메이션 동작\n");
+		printf("적 정찰 시스템 동작\n");
+	}			
 	*/
 
-	// UP-DOWN 게임
-	// 컴퓨터는 1 ~ 50 사이의 난수 값을 가지게 됩니다.
-	srand(time(NULL));
-
-	int answer = 0;
-	int computer = rand() % 50 + 1;
-	int life = 10;
-
-	while(1) // <- 게임 업데이트
+	while (1)
 	{
-		if (life <= 0)
+		// Non Blocking 형태의 키 입력
+		if (GetAsyncKeyState(VK_UP)) // UP
 		{
-			printf("\n목숨을 다 소모하였습니다. (패배)\n");
-			break;
+			printf("↑\n");
+			Sleep(1000); // 1/1000로 계산됩니다.
+			// Sleep() : 특정한 시간을 대기하는 함수
+		}
+		else if (GetAsyncKeyState(VK_LEFT))	// LEFT
+		{
+			printf("←\n");
+			Sleep(100); 
+		}
+		else if (GetAsyncKeyState(VK_RIGHT))	// LEFT
+		{
+			printf("→\n");
+			Sleep(100); 
+		}
+		else if (GetAsyncKeyState(VK_DOWN))	// LEFT
+		{
+			printf("↓\n");
+			Sleep(100);
 		}
 
-		printf("컴퓨터가 가지고 있는 값을 출력해주세요.\n");
-		printf("현재 목숨 : %d", life);
-		printf("\n내가 선택한 값 : ");
-
-		// 서식 지정(정수) []
-		// call by reference (* ???) 
-
-		
-		scanf_s("%s", &answer);
-
-		if (answer < computer)
-		{
-			life--;
-			printf("\ncomputer가 가지고 있는 값보다 작습니다.\n");
-		}
-		else if(answer > computer)
-		{
-			life--;
-			printf("\ncomputer가 가지고 있는 값보다 큽니다.\n");
-		}
-		else if (answer == computer)
-		{
-			printf("\n컴퓨터가 가지고 있는 값을 찾았습니다. (승리)\n");
-	
-		}
+		// printf("게임 애니메이션 동작\n");
+		// printf("적 정찰 시스템 동작\n");
 	}
-	  
 
 	return 0;
 } 
