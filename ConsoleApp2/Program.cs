8b1350c66c3d4d8ec536543265b437d3625bd22d
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-06-16 21:44:41 +0900

    (2023.6.16) 얕은 복사와 깊은 복사

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,31 +1,35 @@
 ﻿using System;
+using System.Collections;
 
 namespace ConsoleApp2
 {
-    [Flags]
-    enum ELEMENT
-    {
-        Water   = 1 << 0,
-        Fire    = 1 << 1,
-        Wind    = 1 << 2,
-        Earth   = 1 << 3,
-        Dark    = 1 << 4,
-        Light   = 1 << 5
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            ELEMENT element = ELEMENT.Water | ELEMENT.Fire;
-            Console.WriteLine(element);
-            Console.WriteLine("불속성이 있나요?");
-            Console.WriteLine((element & ELEMENT.Fire) != 0);
-            Console.WriteLine((element & ELEMENT.Wind) != 0);
+            IEnumerator update = Update();
 
-            Console.WriteLine("불 속성 제거");
-            element ^= ELEMENT.Fire;
-            Console.WriteLine(element);
+            while(true)
+            {
+                if(Console.ReadKey(true).Key == ConsoleKey.A)
+                {
+                    update.MoveNext();
+                    Console.WriteLine(update.Current);
+                }
+
+            }
+        }
+
+        static IEnumerator Update()
+        {
+            int count = 0;
+            while (true)
+            {
+                Console.WriteLine("before atfer 계산");
+                Console.WriteLine("값 대입");
+
+                yield return count++;
+            }
         }
     }
 }
