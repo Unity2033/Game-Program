218c1a0f017b885b861483008774d3e99d5562c4
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-09-26 19:48:32 +0900

    (2023.9.26) 인벤토리 추가 기능

--- Solution/Inventory.cpp
+++ Solution/Inventory.cpp
@@ -2,12 +2,35 @@
 
 Inventory::Inventory(int x, int y)
 {
+	lineX = x;
+	selectIndex = 0;
+
 	size = x * y;
 	items = new Item[size];
 
 	for (int i = 0; i < size; i++)
 	{
 		items[i].SetCheck(false);
+	}
+}
+
+void Inventory::AddItem()
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (items[i].GetCheck() == false)
+		{
+			items[i].SetCheck(true);
+			break;
+		}
+
+		if (i == size - 1)
+		{
+			if (items[size - 1].GetCheck() == true)
+			{
+				std::cout << "인벤토리가 가득 찼습니다~" << std::endl;
+			}
+		}
 	}
 }
 
@@ -24,8 +47,9 @@ void Inventory::Renderer()
 		{
 			std::cout << "□";
 		}
+		else if (items[i].GetCheck() == true)
+		{
+			std::cout << "■";
+		}
 	}
-
-
-
 }
--- Solution/Inventory.h
+++ Solution/Inventory.h
@@ -1,15 +1,22 @@
 #pragma once
 #include "Item.h"
+#include "InputKey.h"
+
 #include <iostream>
 
 class Inventory
 {
 private :
 	int size;
+	int lineX;
+	int selectIndex;
+
 	Item * items;
 
 public :
 	Inventory(int x = 5, int y = 3);
+
+	void AddItem();
 	void Renderer();
 };
 
