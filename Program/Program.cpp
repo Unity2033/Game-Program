0ecd57e04dbbaf08ffb9deff614315b687fe4ffa
Unity2033 <kileab@naver.com>
2024-03-15 21:47:26 +0900

    (2024.3.15) 원형 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,89 +2,8 @@
 
 using namespace std;
 
-template <typename T>
-class DoubleLinkedList
-{
-private:
-	struct Node
-	{
-		T data;
-		Node * next;
-		Node * prev;
-	};
-
-	int size;
-	Node * head;
-	Node * tail;
-
-public:
-
-	DoubleLinkedList()
-	{
-		size = 0;
-		head = nullptr;
-		tail = nullptr;
-	}
-
-	void PushFront(T data)
-	{
-		Node * newNode = new Node;
-	
-		newNode->data = data;
-
-		newNode->next = nullptr;
-
-		newNode->prev = nullptr;
-
-		if (head == nullptr)
-		{
-			head = newNode;
-			tail = newNode;
-		}
-		else
-		{
-			newNode->next = head;
-
-			head->prev = newNode;
-
-			head = newNode;
-		}
-
-		size++;
-	}
-
-	void PushBack(T data)
-	{
-		Node * newNode = new Node;
-
-		newNode->data = data;
-
-		newNode->next = nullptr;
-
-		newNode->prev = nullptr;
-
-		if (head == nullptr)
-		{
-			head = newNode;
-			tail = newNode;
-		}
-		else
-		{
-			newNode->prev = tail;
-
-			tail->next = newNode;
-
-			tail = newNode;
-		}
-
-		size++;
-	}
-
-};
-
 int main()
 {
-
 
 	return 0;
 }
