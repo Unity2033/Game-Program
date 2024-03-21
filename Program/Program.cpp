88968e91482b6278ba870b0408df3da33f989f30
Unity2033 <kileab@naver.com>
2024-03-21 21:45:45 +0900

    (2024.3.21) 벡터

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,98 +2,118 @@
 
 using namespace std;
 
-class Vector2
+template<typename T>
+class Vector
 {
+private:
+	int size;
+	int capacity;
+	T * bufferPointer;
+
 public:
-	int x;
-	int y;
-
-	Vector2(int x, int y)
+	Vector()
 	{
-		this->x = x;
-		this->y = y;
+		size = 0;
+		capacity = 0;
+		bufferPointer = nullptr;
 	}
 
-	Vector2 operator+(const Vector2 & vector2)
+	void PushBack(T data)
 	{
-		Vector2 other
-		(
-			this->x + vector2.x,
-			this->y + vector2.y
-		);
+		if (capacity == 0)
+		{
+			Resize(1);
+		}
+		else if(size >= capacity)
+		{
+			Resize(capacity * 2);
+		}
 
-		return other;
+		bufferPointer[size++] = data;
 	}
 
-	Vector2 operator-(const Vector2& vector2)
+	void PopBack()
 	{
-		Vector2 other
-		(
-			this->x - vector2.x,
-			this->y - vector2.y
-		);
-
-		return other;
+		if (size <= 0)
+		{
+			cout << "Vector is Empty" << endl;
+		}
+		else
+		{
+			bufferPointer[--size] = NULL;
+		}
 	}
 
-	Vector2 operator*(const Vector2& vector2)
+	void Resize(int newSize)
 	{
-		Vector2 other
-		(
-			this->x * vector2.x,
-			this->y * vector2.y
-		);
+		// 1. capacity에 새로운 size값을 설정합니다.
+		capacity = newSize;
 
-		return other;
+		// 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
+		//    미모리 공간을 가리키도록 합니다.
+		T * newPointer = new T[capacity];
+
+		// 3. 새로운 메모리 공간의 값을 초기화합니다.
+		for (int i = 0; i < capacity; i++)
+		{
+			newPointer[i] = NULL;
+		}
+
+		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열에
+		//    넣어줍니다.
+		for (int i = 0; i < size; i++)
+		{
+			newPointer[i] = bufferPointer[i];
+		}
+
+		// 5. bufferPointer의 메모리 주소를 해제합니다.
+		if (bufferPointer != nullptr)
+		{
+			delete [ ] bufferPointer;
+		}
+
+		// 6. bufferPointer에 새로운 메모리 주소를 저장합니다.
+		bufferPointer = newPointer;
 	}
 
-	Vector2 operator*(int value)
+	void Reserve(int newSize)
 	{
-		Vector2 other
-		(
-			this->x * value,
-			this->y * value
-		);
+		if (newSize < capacity)
+		{
+			return;
+		}
 
-		return other;
+		Resize(newSize);
 	}
 
-	Vector2 operator/(const Vector2& vector2)
+	T & operator[](const int & index)
 	{
-		Vector2 other
-		(
-			this->x / vector2.x,
-			this->y / vector2.y
-		);
+		return bufferPointer[index];
+	}
 
-		return other;
+	int& Size()
+	{
+		return size;
+	}
+
+	~Vector()
+	{
+		if (bufferPointer != nullptr)
+		{
+			delete[] bufferPointer;
+		}
 	}
 };
 
-Vector2 operator*(int value, Vector2 vector2)
-{
-	return vector2 * value;
-}
-
 int main()
 {
-	Vector2 vector1(10,20);
-	Vector2 vector2(5, 5);
+	Vector<int> vector;
 
-	Vector2 vector3 = vector1 / vector2;
+	vector.PushBack(10);
+	vector.PushBack(20);
 
-	cout << "vector3의 x 값 : " << vector3.x << endl;
-	cout << "vector3의 y 값 : " << vector3.y << endl;
 
-	vector3 = vector1 * 5;
-
-	cout << "vector3의 x 값 : " << vector3.x << endl;
-	cout << "vector3의 y 값 : " << vector3.y << endl;
-
-	vector3 = 10 * vector1 ;
-
-	cout << "vector3의 x 값 : " << vector3.x << endl;
-	cout << "vector3의 y 값 : " << vector3.y << endl;
+	cout << vector[0];
 
 	return 0;
 }
