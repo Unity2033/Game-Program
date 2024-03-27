76adfaed902cfa0bf6a6de25a8e20c4515007af6
Unity2033 <kileab@naver.com>
2024-03-27 21:46:39 +0900

    (2024.3.27) 해시 테이블 (체이닝 기법)

---
+++ Class11th (Heap)/Source.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+
+#define SIZE 8
+
+using namespace std;
+
+template <typename T>
+class Heap
+{
+private:
+	T buffer[SIZE];
+	int index;
+
+public:
+	Heap()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			buffer[i] = NULL;
+		}
+
+		index = 0;
+	}
+
+	void Insert(T data)
+	{
+		if (index >= SIZE - 1)
+		{
+			cout << "Heap is Full" << endl;
+			return;
+		}
+
+		buffer[++index] = data;
+
+		int child = index;
+		int parent = index / 2;
+
+		while (child > 1)
+		{
+			// 자식 노드와 부모 노드의 데이터를 비교합니다.
+			if (buffer[parent] < buffer[child])
+			{
+				swap(buffer[parent], buffer[child]);
+			}
+
+			child = parent;
+			parent = child / 2;
+		}
+	}
+
+	T& Delete()
+	{
+		// 1. Heap이 비어있다면 프로그램을 종료합니다.
+		if (index <= 0)
+		{
+			cout << "Heap is Empty" << endl;
+			exit(1);
+		}
+
+		// 2. 임시 변수에 buffer[1] 값을 보관합니다.
+		T result = buffer[1];
+
+		// 3. index로 가리키는 배열의 값을 첫 번째 원소에 넣어줍니다.
+		buffer[1] = buffer[index];
+
+		// 4. index로 가리키는 배열의 값을 초기화합니다.
+		buffer[index] = NULL;
+
+		// 5. index의 값을 감소합니다.
+		index--;
+
+		int parent = 1;
+
+		while (parent * 2 <= index)
+		{
+			int child = parent * 2;
+
+			// 오른쪽 자식의 노드가 클 때
+			if (buffer[child] < buffer[child + 1])
+			{
+				child++;
+			}
+
+			// 부모 노드의 key 값이 자식 노드의 key 값보다 크다면
+			// 반복을 종료합니다.
+			if (buffer[child] < buffer[parent])
+			{
+				break;
+			}
+
+			swap(buffer[parent], buffer[child]);
+			parent = child;
+		}
+
+		return result;
+	}
+
+	void Show()
+	{
+		for (T element : buffer)
+		{
+			cout << element << " ";
+		}
+	}
+};
+
+int main()
+{
+	Heap<int> heap;
+
+	heap.Insert(1);
+	heap.Insert(2);
+	heap.Insert(3);
+	heap.Insert(4);
+	heap.Insert(5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		heap.Show();
+
+		cout << heap.Delete() << endl;
+	}
+
+	cout << heap.Delete() << endl;
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,87 +1,82 @@
 ﻿#include <iostream>
 
-#define SIZE 8
+#define SIZE 6
 
 using namespace std;
 
-template <typename T>
-class Heap
+template<typename KEY, typename VALUE>
+class HashTable
 {
-private :
-	T buffer[SIZE];
-	int index;
+private:
+	struct Node
+	{
+		KEY key;
+		VALUE value;
 
-public :
-	Heap()
+		Node * next;
+	};
+
+	struct Bucket
+	{
+		int count;
+		Node * head;
+	};
+
+	Bucket bucket[SIZE];
+public:
+	HashTable()
 	{
 		for (int i = 0; i < SIZE; i++)
 		{
-			buffer[i] = NULL;
-		}
-
-		index = 0;
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
+		}		   
 	}
 
-	void Insert(T data)
+	template<typename T>
+	int HashFunction(T key)
 	{
-		if (index >= SIZE - 1)
-		{
-			cout << "Heap is Full" << endl;
-			return;
-		}
+		int hashIndex = (int)key % SIZE;
 
-		buffer[++index] = data;
-
-		int child = index;
-		int parent = index / 2;
-
-		while (child > 1)
-		{
-			// 자식 노드와 부모 노드의 데이터를 비교합니다.
-			if (buffer[parent] < buffer[child])
-			{
-				swap(buffer[parent], buffer[child]);
-			}
-
-			child = parent;
-			parent = child / 2;
-		}
+		return hashIndex;
 	}
 
-	T & Delete()
-	{
-		// 1. Heap이 비어있다면 프로그램을 종료합니다.
-		if (index <= 0)
-		{
-			cout << "Heap is Empty" << endl;
-			exit(1);
-		}
+	// HashFunction 템플릿 특수화
 
-		// 2. 임시 변수에 buffer[1] 값을 보관합니다.
-		T result = buffer[1];
 
-		// 3. index로 가리키는 배열의 값을 첫 번째 원소에 넣어줍니다.
-		buffer[1] = buffer[index];
 
-		// 4. index로 가리키는 배열의 값을 초기화합니다.
-		buffer[index] = NULL:
-
-		// 5. index의 값을 감소합니다.
-		index--;
-
-	}
-
-	void Show()
-	{
-		for (T element : buffer)
-		{
-			cout << element << " ";
-		}
-	}
 };
 
 int main()
 {
+#pragma region 해시 테이블
+	// (Key, Value)로 데이터를 저장하는 자료 구조 중 하나로
+	// 빠르게 데이터를 검색할 수 있는 자료 구조입니다.
+
+	// 해시 충돌을 해결하는 방법
+
+	// <체이닝 기법>
+	// 각 해시 버킷을 연결 리스트로 구성하는 방식입니다.
+
+	// 해시 충돌 발생 시 동일한 해시 값에 해당하는 데이터들을
+	// 연결리스트로 연결하여 저장합니다.
+
+	// <개방 주소법>
+	// 충돌 발생 시 빈 버켓에 데이터를 저장하는 방식입니다.
+
+	// 빈 버킷을 어떻게 결정할 지에 따라 구현 방식이 달라집니다.
+
+	// 선형 탐사 : 충돌 발생 시 앞에서 부터 차례대로 빈 버킷을
+	// 찾아 저장하는 방식입니다.
+
+	// 이차 탐사 : 충돌 발생 시 2^, 2^3 만큼 떨어진 빈 버킷을 
+	// 찾아 값을 저장하는 방식입니다.
+
+	// 이중 해싱 : 해시 값을 한번 더 해시 함수에서 다른 함수를
+	// 도출하는 방식입니다.
+#pragma endregion
+
+
 
 	return 0;
 }
