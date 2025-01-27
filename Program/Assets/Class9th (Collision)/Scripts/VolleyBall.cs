fae51b8f71ef0a054c46d35ea3745404e9e28a4d
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-01-27 21:43:33 +0900

    (2025.1.27) 광선 검사 시스템

---
+++ Program/Assets/Class9th (Collision)/Scripts/VolleyBall.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolleyBall : MonoBehaviour, IDamageable
+{
+    public void Use()
+    {
+        Debug.Log("Volley Ball");
+    }
+}
