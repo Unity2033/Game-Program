94954d9293793c342c94817072822fb112e3caf3
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-02-03 21:44:06 +0900

    (2025.2.3) 애니메이션 레이어

---
+++ Program/Assets/Class10th (Ray Cast)/Scripts/Picking.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picking : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit rayCastHit;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out rayCastHit, Mathf.Infinity))
+            {
+                rayCastHit.transform.GetComponent<View>().Show();
+            }
+        }
+    }
+}
