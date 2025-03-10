9a7f258a6b54fd4ff9b7e0ec8cec7f2e6f619603
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-03-10 21:44:33 +0900

    (2025.3.10) 셰이더 적용

---
+++ Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float time;
+    [SerializeField] int minute;
+    [SerializeField] int second;
+    [SerializeField] int milliseconds;
+
+    [SerializeField] Text textTime;
+
+    private void Awake()
+    {
+        textTime = GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(Watch());
+    }
+
+    IEnumerator Watch()
+    {
+        while (GameManager.Instance.State)
+        {
+            time += Time.deltaTime;
+
+            minute = (int)time / 60;
+            second = (int)time % 60;
+            milliseconds = (int)(time * 100) % 100;
+
+            textTime.text = string.Format("{0:D2} : {1:D2} : {2:D2}", minute, second, milliseconds);
+
+            yield return null;
+        }
+    }
+}
