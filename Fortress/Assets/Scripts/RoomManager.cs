0a4e90ec3ef1e9b93ef28ce86e4481be9af03344
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-03-17 21:43:46 +0900

    (2025.3.17) 로비 접속 시스템

---
+++ Fortress/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField titleInputField;
+    [SerializeField] InputField capacityInputField;
+
+    [SerializeField] Transform parentTransform;
+
+    void Start()
+    {
+        
+    }
+
+}
