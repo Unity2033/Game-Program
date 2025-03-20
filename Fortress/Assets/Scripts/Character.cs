3431434cf1162d6c575287dd18e50d0dd1393afc
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-03-20 21:44:00 +0900

    (2025.3.20) 마우스 관리자 시스템

--- Fortress/Assets/Scripts/Character.cs
+++ Fortress/Assets/Scripts/Character.cs
@@ -2,16 +2,36 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(Move))]
 public class Character : MonoBehaviourPun
 {
+    [SerializeField] Move move;
+    [SerializeField] Rigidbody rigidBody;
     [SerializeField] GameObject remoteCamera;
+
+    private void Awake()
+    {
+        move = GetComponent<Move>();
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
     void Start()
     {
         DisableCamera();
     }
-    
+
+    private void Update()
+    {
+        move.OnKeyUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        move.OnMove(rigidBody);
+    }
+
     public void DisableCamera()
     {
         // 현재 플레이어가 나 자신이라면?
