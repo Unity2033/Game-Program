d713ccac0b352f3058fa7f68a2d66e5a1a51501a
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-04-22 19:44:22 +0900

    (2025.4.22) 이동 시스템

---
+++ Game/Assets/Class8th (Movement)/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position = new Vector3(1, 1, 1);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+
+    }
+}
