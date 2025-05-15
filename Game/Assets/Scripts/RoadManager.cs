fdaa7162ffc8d89d722aefc4b3b473596e6670f9
Unity2033 <kileab@naver.com>
2025-05-15 19:44:49 +0900

    (2025.5.15) 도로 위치 초기화 시스템

--- Game/Assets/Scripts/RoadManager.cs
+++ Game/Assets/Scripts/RoadManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
 public class RoadManager : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float offset = 40.0f;
     [SerializeField] List<GameObject> roads;
 
     void Update()
@@ -13,5 +15,18 @@ public class RoadManager : MonoBehaviour
         {
             roads[i].transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
+    }
+
+    public void InitializePosition()
+    {
+        GameObject newRoad = roads[0];
+
+        roads.Remove(newRoad);
+
+        float newZ = roads[roads.Count - 1].transform.position.z + offset;
+
+        newRoad.transform.position = new Vector3(0, 0, newZ);
+
+        roads.Add(newRoad);
     }
 }
