4a9dd0be36d21b74bb20fc153f0015051efda8db
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-05-21 19:44:51 +0900

    (2025.5.21) 마우스 관리자 시스템

--- Game/Assets/Scripts/ObstacleManager.cs
+++ Game/Assets/Scripts/ObstacleManager.cs
@@ -33,6 +33,19 @@ public class ObstacleManager : MonoBehaviour
         }
     }
 
+    bool ExamineActive()
+    {
+        for(int i = 0; i < obstacles.Count; i++)
+        {
+            if (obstacles[i].activeSelf == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public IEnumerator ActiveObstacle()
     {
         while(true)
@@ -42,6 +55,18 @@ public class ObstacleManager : MonoBehaviour
             // 현재 게임 오브젝트가 활성화되어 있는 지 확인합니다.
             while (obstacles[random].activeSelf == true)
             {
+                // 현재 리스트에 있는 모든 게임 오브젝트가 활성화되어 있는 지 확인합니다.
+                if(ExamineActive())
+                {
+                    // 모든 게임 오브젝트가 활성화되어 있다면 게임 오브젝트를 새로
+                    // 생성한 다음 obstacles 리스트에 넣어줍니다.
+                    GameObject clone = Instantiate(prefab[Random.Range(0, prefab.Length)], transform);
+
+                    clone.SetActive(false);
+
+                    obstacles.Add(clone);
+                }
+
                 // 현재 인덱스에 있는 게임 오브젝트가 활성화되어 있으면
                 // random 변수의 값을 +1을 해서 다시 검색합니다.
                 random = (random + 1) % obstacles.Count;
