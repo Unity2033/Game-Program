cb52594e1dd784bdde6712e405ce118d5ae330c5
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-08-19 19:45:56 +0900

    (2025.8.19) 좌표 설정 시스템

---
+++ Class3th (Input System)/Source.c
@@ -0,0 +1,60 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+#define SIZE 10
+
+int main()
+{
+#pragma region GetAsyncKeyState
+	// Windows API에서 제공하는 입력 처리 함수로, 지정된 키의 상태를
+	// 비동적으로 확인할 때 사용되는 함수입니다.
+
+	// Key State
+
+	// 0x0000 : 이전에 누른 적이 없고, 호출 시점에도 눌려있지 않은 상태
+
+	// 0x0001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있지 않은 상태
+
+	// 0x8000 : 이전에 누른 적이 없고, 호출 시점에는 눌려있는 상태
+
+	// 0x8001 : 이전에 누른 적이 있고, 호출 시점에도 눌려있는 상태
+
+	const char* dialog[SIZE];
+
+	dialog[0] = "안녕하세요...?";
+	dialog[1] = "누구신가요...?";
+	dialog[2] = "탐정입니다.";
+	dialog[3] = "저는 의뢰인입니다.";
+	dialog[4] = "수상한 사건이 있다고 해서 왔습니다.";
+	dialog[5] = "네 맞습니다.";
+	dialog[6] = "이 사건에 대해 아시는 게 있나요.?";
+	dialog[7] = "조금 알고 있습니다.";
+	dialog[8] = "말씀해주세요.";
+	dialog[9] = "사실은... ☏";
+
+	int i = 0;
+
+	while (i < SIZE)
+	{
+		if (GetAsyncKeyState(VK_SPACE) & 0x0001)
+		{
+			if (i % 2 == 0)
+			{
+				printf("탐정 : %s\n", dialog[i]);
+			}
+			else
+			{
+				printf("의뢰인 : %s\n", dialog[i]);
+			}
+
+			i++;
+		}
+	}
+
+	printf("대화가 종료되었습니다.\n");
+
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Program.c
+++ Program/Program.c
@@ -1,38 +1,25 @@
 ﻿#include <stdio.h>
+#include <windows.h>
 
-#define SIZE 10
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+void move(int x, int y)
+{
+	// x와 y축을 설정하는 구조체입니다.
+	COORD position = { x, y };
+
+	// 콘솔 커서의 좌표를 설정하는 함수입니다.
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
 
 int main()
 {
-#pragma region GetAsyncKeyState
-	// Windows API에서 제공하는 입력 처리 함수로, 지정된 키의 상태를
-	// 비동적으로 확인할 때 사용되는 함수입니다.
+	move(5, 5);
 
-	// Key State
-	
-	// 0x0000 : 이전에 누른 적이 없고, 호출 시점에도 눌려있지 않은 상태
-	
-	// 0x0001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있지 않은 상태
-
-	// 0x8000 : 이전에 누른 적이 없고, 호출 시점에는 눌려있는 상태
-
-	// 0x8001 : 이전에 누른 적이 있고, 호출 시점에도 눌려있는 상태
-
-	const char * dialog[SIZE];
-
-	dialog[0] = "안녕하세요...?";
-	dialog[1] = "누구신가요...?";
-	dialog[2] = "탐정입니다.";
-	dialog[3] = "저는 의뢰인입니다.";
-	dialog[4] = "수상한 사건이 있다고 해서 왔습니다.";
-	dialog[5] = "네 맞습니다.";
-	dialog[6] = "이 사건에 대해 아시는 게 있나요.?";
-	dialog[7] = "조금 알고 있습니다.";
-	dialog[8] = "말씀해주세요.";
-	dialog[9] = "사실은... ☏";
-
-#pragma endregion
-
-
+	printf("★");
+		
 	return 0;
 }
