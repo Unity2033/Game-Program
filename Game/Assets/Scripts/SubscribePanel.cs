8aa99fb6cc4c32c76e51438709404d1eb5a6c673
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-11-17 21:44:18 +0900

    (2025.11.17) 회원 가입 시스템

---
+++ Game/Assets/Scripts/SubscribePanel.cs
@@ -0,0 +1,40 @@
+using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubscribePanel : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField nameInputField;
+    [SerializeField] InputField addressInputField;
+    [SerializeField] InputField passwordInputField;
+
+    public void Subscribe()
+    {
+        var request = new RegisterPlayFabUserRequest
+        { 
+           Email = addressInputField.text,
+           Password = passwordInputField.text,
+           Username = nameInputField.text, 
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            request,
+            Success,
+            Failure
+        );
+    }
+
+    public void Success(RegisterPlayFabUserResult registerPlayFabUserResult)
+    {
+        Debug.Log(registerPlayFabUserResult.Username);
+    }
+
+    public void Failure(PlayFabError playFabError)
+    {
+        PanelManager.Instance.Load(Panel.Error, playFabError.GenerateErrorReport());
+    }
+
+}
