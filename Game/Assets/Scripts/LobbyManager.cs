dba7cc64b83cd2ecc8a97dff4090e3bfb4c811a9
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-11-19 21:43:33 +0900

    (2025.11.19) 룸 시스템

--- Game/Assets/Scripts/LobbyManager.cs
+++ Game/Assets/Scripts/LobbyManager.cs
@@ -5,12 +5,50 @@ using UnityEngine;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] Transform parentTransform;
+
     [SerializeField] Dictionary<string, GameObject> dictionary = new();
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
         GameObject prefab = null;
 
+        foreach(RoomInfo roomInfo in roomList)
+        {
+            // 룸이 삭제된 경우
+            if(roomInfo.RemovedFromList == true)
+            {
+                dictionary.TryGetValue(roomInfo.Name, out prefab);
+
+                Destroy(prefab);
+
+                dictionary.Remove(roomInfo.Name);
+            }
+            else // 룸의 정보가 변경되는 경우
+            {
+                // 룸이 처음 생성되는 경우
+                if (dictionary.ContainsKey(roomInfo.Name) == false)
+                {
+                    GameObject clone = Instantiate(Resources.Load<GameObject>("Room"), parentTransform);
+
+                    clone.GetComponent<RoomView>().UpdateRoomInformation(roomInfo);
+
+                    dictionary.Add(roomInfo.Name, clone);
+                }
+                else
+                {
+                    dictionary.TryGetValue(roomInfo.Name, out prefab);
+
+                    prefab.GetComponent<RoomView>().UpdateRoomInformation(roomInfo);
+                }
+            }
+
+        }
 
     }
 
