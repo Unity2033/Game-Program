2f863dac74bb77ff83ba28ce6da17648d81f9113
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-12-02 21:43:59 +0900

    (2025.12.2) 닉네임 시스템

---
+++ Game/Assets/Scripts/MasterManager.cs
@@ -0,0 +1,31 @@
+using Photon.Pun;
+using UnityEngine;
+using Photon.Realtime;
+using System.Collections;
+
+public class MasterManager : MonoBehaviourPunCallbacks
+{
+    private WaitForSeconds waitForSeconds = new WaitForSeconds(5);
+
+    public IEnumerator Start()
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            while(true)
+            {
+                PhotonNetwork.InstantiateRoomObject("Ball", Vector3.zero, Quaternion.identity); 
+
+                yield return waitForSeconds;
+            }
+        }
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        PhotonNetwork.SetMasterClient(PhotonNetwork.PlayerList[0]);
+
+        Debug.Log(PhotonNetwork.PlayerList[0]);
+    }
+
+
+}
