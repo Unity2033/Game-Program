f43db2db64ebcc17f336463af538f2c512881be5
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-12-16 19:45:50 +0900

    (2025.12.16) 유저 인터페이스

---
+++ Program/Assets/Class5th (User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Execute()
+    {
+        Debug.Log("Start");
+    }
+
+    public void Shop()
+    {
+        Debug.Log("Shop");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+    }
+
+}
