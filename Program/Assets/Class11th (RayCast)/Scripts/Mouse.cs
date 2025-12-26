0bcfd28ca844a43ad5f9b43973ee6af2e73b9911
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-12-26 19:46:01 +0900

    (2025.12.26) 광선 검출

---
+++ Program/Assets/Class11th (RayCast)/Scripts/Mouse.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Scripting;
+
+public class Mouse : MonoBehaviour
+{
+    [SerializeField] Ray ray;
+    [SerializeField] RaycastHit raycastHit;
+
+    [SerializeField] float duration = 1.0f;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+            {
+                Debug.DrawLine(ray.origin, raycastHit.point, Color.green, duration);
+            }      
+        }
+    }
+}
