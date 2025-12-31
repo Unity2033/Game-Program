818e6e859950e7627885e8fe824935e7a04ac0a9
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-12-31 19:44:43 +0900

    (2025.12.31) 조명 시스템

--- Program/Assets/Class12th (Coroutine)/Scripts/CreateManager.cs
+++ Program/Assets/Class12th (Coroutine)/Scripts/CreateManager.cs
@@ -6,21 +6,27 @@ public class CreateManager : MonoBehaviour
     [SerializeField] float time;
     [SerializeField] GameObject prefab;
 
-    private void Update()
+    [SerializeField] WaitForSeconds waitForSeconds;
+
+    private void Awake()
     {
-        time += Time.deltaTime;
-
-        if(time >= 5.0f)
-        {
-            GameObject clone = Instantiate(prefab);
-
-            clone.transform.rotation = Quaternion.Euler(0f, 180.0f, 0f);
-
-            time = 0.0f;
-        }
+        waitForSeconds = new WaitForSeconds(time);
     }
 
-     
+    private void Start()
+    {
+        StartCoroutine(Coroutine());
+    }
 
+    IEnumerator Coroutine()
+    {
+        while (true)
+        {
+            yield return waitForSeconds;
 
+            GameObject clone = Instantiate(prefab, gameObject.transform);
+
+            clone.transform.rotation = Quaternion.Euler(0f, 180.0f, 0f);
+        }
+    }
 }
