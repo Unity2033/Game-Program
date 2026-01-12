52d0daf15bea24779b1e1623f870d262681d24a7
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-01-12 19:44:29 +0900

    (2026.1.12) 디자인 패턴

--- Program/Assets/Class11th (RayCast)/Scripts/Equipment.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class Equipment : MonoBehaviour
-{
-    public abstract void Operate();
-}
--- Program/Assets/Class11th (RayCast)/Scripts/Tent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Tent : Equipment
-{
-    public override void Operate()
-    {
-        transform.Rotate(0, 15, 0);
-    }
-}
