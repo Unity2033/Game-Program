357cb0c19bc4f6174edb38903445a5eec3c588b1
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-01-16 19:46:50 +0900

    (2026.1.16) 관찰자 패턴

---
+++ Program/Assets/Strategy Pattern/Scripts/WeaponManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField] int count;
+    [SerializeField] List<Weapon> weapons;
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Swap();
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            weapons[count].Attack();
+        }
+    }
+
+    public void Swap()
+    {
+        weapons[count].gameObject.SetActive(false);
+
+        count = (count + 1) % weapons.Count;
+
+        weapons[count].gameObject.SetActive(true);
+    }
+}
