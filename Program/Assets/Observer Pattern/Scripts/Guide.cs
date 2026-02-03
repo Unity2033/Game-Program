499a5226d888179d6179a6a118a72a33cfd3d2fd
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-02-03 19:44:36 +0900

    (2026.2.3) 리스코프 치환 원칙

---
+++ Program/Assets/Observer Pattern/Scripts/Guide.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Guide : MonoBehaviour
+{
+    [SerializeField] Quest quest;
+    [SerializeField] Canvas questCanvas;
+
+    private void Awake()
+    {
+        questCanvas = transform.GetChild(0).GetComponent<Canvas>();
+    }
+
+    public void Accept()
+    {
+        QuestManager.Instance.Accept(quest);
+
+        questCanvas.gameObject.SetActive(false);
+    }
+
+}
--- Program/Assets/Observer Pattern/Scripts/QuestManager.cs
+++ Program/Assets/Observer Pattern/Scripts/QuestManager.cs
@@ -1,7 +1,20 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestManager : MonoBehaviour
+public class QuestManager : Singleton<QuestManager>
 {
     public static event Action<Quest> OnQuestCompleted;
+
+    [SerializeField] List<Quest> questList = new List<Quest>();
+
+    public void Accept(Quest quest)
+    {
+        if(quest == null || questList.Contains(quest))
+        {
+            return;
+        }
+
+        questList.Add(quest);
+    }
 }
